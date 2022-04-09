2e22b3e2f24184f77998f6061148c1449c752e8a
Nolan <dartoisn@hotmail.Fr>
2022-04-10 00:44:55 +0200

    feat(User): Add methods in UserRepository

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -62,6 +62,21 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $this->_em->flush();
     }
 
+    public function getUserByRole(string $role)
+    {
+        $role = mb_strtoupper($role);
+
+        return $this->createQueryBuilder('u')
+            ->andWhere('JSON_CONTAINS(u.roles, :role) = 1')
+            ->setParameter('role', '"ROLE_'.$role.'"')
+            ->getQuery()->getResult();
+    }
+
+    public function getAllUsers()
+    {
+        return $this->createQueryBuilder('u')->getQuery()->getResult();
+    }
+
     // /**
     //  * @return User[] Returns an array of User objects
     //  */
