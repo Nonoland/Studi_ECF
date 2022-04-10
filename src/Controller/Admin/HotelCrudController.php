3516fe4ca999b0f6efdc852a692c06f7510e614a
Nolan <dartoisn@hotmail.Fr>
2022-04-10 17:04:31 +0200

    feat(Admin): set Hotel Crud

--- src/Controller/Admin/HotelCrudController.php
+++ src/Controller/Admin/HotelCrudController.php
@@ -4,14 +4,16 @@ namespace App\Controller\Admin;
 
 use App\Entity\Hotel;
 use App\Entity\User;
+use Ausi\SlugGenerator\SlugOptions;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Ausi\SlugGenerator\SlugGenerator;
 
 class HotelCrudController extends AbstractCrudController
 {
@@ -27,7 +29,7 @@ class HotelCrudController extends AbstractCrudController
             TextField::new('name'),
             TextEditorField::new('description'),
             TextField::new('address'),
-            TextField::new('complement'),
+            TextField::new('complement')->setRequired(false),
             TextField::new('zipcode'),
             TextField::new('city'),
             AssociationField::new('users')->setQueryBuilder(
@@ -39,6 +41,17 @@ class HotelCrudController extends AbstractCrudController
     public function configureActions(Actions $actions): Actions
     {
         return $actions->add(Crud::PAGE_INDEX, 'detail');
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $slugGenerator = new SlugGenerator((new SlugOptions)
+            ->setLocale('fr')
+            ->setDelimiter('_')
+            ->setValidChars('a-zA-Z0-9')
+        );
+        $entityInstance->setSlug($slugGenerator->generate($entityInstance->getName()));
+        parent::persistEntity($entityManager, $entityInstance);
     }
 
 }
