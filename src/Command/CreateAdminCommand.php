50acc5e53be0c8ed8f22db30c9d68a6e82cda095
Nolan <dartoisn@hotmail.Fr>
2022-04-20 16:36:38 +0200

    feat(Command): Add Command for create admin account

---
+++ src/Command/CreateAdminCommand.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-admin',
+    description: 'Add a short description for your command',
+)]
+class CreateAdminCommand extends Command
+{
+
+    private EntityManagerInterface $entityManager;
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher)
+    {
+        parent::__construct();
+
+        $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::OPTIONAL, 'User email')
+            ->addArgument('password', InputArgument::OPTIONAL, 'User password')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+
+        $admin = new User();
+
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $admin,
+            $password
+        );
+        $admin->setPassword($hashedPassword);
+
+        $admin->setEmail($email);
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $admin->setFirstname('Admin');
+        $admin->setLastname('Admin');
+
+        $this->entityManager->persist($admin);
+        $this->entityManager->flush();
+
+        $io->success('Admin account successful created !');
+
+        return Command::SUCCESS;
+    }
+}
